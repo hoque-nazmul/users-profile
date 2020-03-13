63c6441d95369ba691d6df2abe688683afa03a47
Nazmul Hoque <coder.nazmul@gmail.com>
2020-03-13 18:40:03 +0600

    header component is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <div className="left">
+                <h2>Users Information</h2>
+            </div>
+            <div className="right">
+                <a href="/home">Home</a>
+                <a href="/profile">Profile</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
