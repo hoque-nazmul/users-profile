61a6c7f423f95853b6b7da19704f6632384c18ab
Nazmul Hoque <coder.nazmul@gmail.com>
2020-03-13 16:49:07 +0600

    users data resourses is added

---
+++ src/userData/user.js
@@ -0,0 +1,369 @@
+var users = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "img" : "http://4.bp.blogspot.com/-NahUaexOqVU/T8mVxrxkWSI/AAAAAAAAABE/ZWGO81RLs30/s1600/beautiful-old-people-photography-part-31.jpg",
+    "salary" : 500000,
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "img" : "https://resources.stuff.co.nz/content/dam/images/1/d/2/j/1/5/image.related.StuffLandscapeSixteenByNine.1420x800.1d2j1s.png/1469340205998.jpg",
+    "salary" : 500000,
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  },
+  {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "img" : "http://www.top43dprinting.com/wp-content/uploads/2014/07/Picture-of-person.png",
+    "salary" : 500000,
+    "email": "Nathan@yesenia.net",
+    "address": {
+      "street": "Douglas Extension",
+      "suite": "Suite 847",
+      "city": "McKenziehaven",
+      "zipcode": "59590-4157",
+      "geo": {
+        "lat": "-68.6102",
+        "lng": "-47.0653"
+      }
+    },
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "company": {
+      "name": "Romaguera-Jacobson",
+      "catchPhrase": "Face to face bifurcated interface",
+      "bs": "e-enable strategic applications"
+    }
+  },
+  {
+    "id": 4,
+    "name": "Patricia Lebsack",
+    "username": "Karianne",
+    "img" : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pierre-Person.jpg/220px-Pierre-Person.jpg",
+    "salary" : 500000,
+    "email": "Julianne.OConner@kory.org",
+    "address": {
+      "street": "Hoeger Mall",
+      "suite": "Apt. 692",
+      "city": "South Elvis",
+      "zipcode": "53919-4257",
+      "geo": {
+        "lat": "29.4572",
+        "lng": "-164.2990"
+      }
+    },
+    "phone": "493-170-9623 x156",
+    "website": "kale.biz",
+    "company": {
+      "name": "Robel-Corkery",
+      "catchPhrase": "Multi-tiered zero tolerance productivity",
+      "bs": "transition cutting-edge web services"
+    }
+  },
+  {
+    "id": 5,
+    "name": "Chelsey Dietrich",
+    "username": "Kamren",
+    "img" : "https://marconisida.com/wp-content/uploads/2015/02/frustrated-person-400x284.png",
+    "salary" : 500000,
+    "email": "Lucio_Hettinger@annie.ca",
+    "address": {
+      "street": "Skiles Walks",
+      "suite": "Suite 351",
+      "city": "Roscoeview",
+      "zipcode": "33263",
+      "geo": {
+        "lat": "-31.8129",
+        "lng": "62.5342"
+      }
+    },
+    "phone": "(254)954-1289",
+    "website": "demarco.info",
+    "company": {
+      "name": "Keebler LLC",
+      "catchPhrase": "User-centric fault-tolerant solution",
+      "bs": "revolutionize end-to-end systems"
+    }
+  },
+  {
+    "id": 6,
+    "name": "Mrs. Dennis Schulist",
+    "username": "Leopoldo_Corkery",
+    "img" : "https://news.nd.edu/assets/113411/james_brockmole_300.jpg",
+    "salary" : 500000,
+    "email": "Karley_Dach@jasper.info",
+    "address": {
+      "street": "Norberto Crossing",
+      "suite": "Apt. 950",
+      "city": "South Christy",
+      "zipcode": "23505-1337",
+      "geo": {
+        "lat": "-71.4197",
+        "lng": "71.7478"
+      }
+    },
+    "phone": "1-477-935-8478 x6430",
+    "website": "ola.org",
+    "company": {
+      "name": "Considine-Lockman",
+      "catchPhrase": "Synchronised bottom-line interface",
+      "bs": "e-enable innovative applications"
+    }
+  },
+  {
+    "id": 7,
+    "name": "Kurtis Weissnat",
+    "username": "Elwyn.Skiles",
+    "img" : "http://blogs.uww.edu/brada252/files/2016/10/hostgator-alternative-frustrated-person.jpg",
+    "salary" : 500000,
+    "email": "Telly.Hoeger@billy.biz",
+    "address": {
+      "street": "Rex Trail",
+      "suite": "Suite 280",
+      "city": "Howemouth",
+      "zipcode": "58804-1099",
+      "geo": {
+        "lat": "24.8918",
+        "lng": "21.8984"
+      }
+    },
+    "phone": "210.067.6132",
+    "website": "elvis.io",
+    "company": {
+      "name": "Johns Group",
+      "catchPhrase": "Configurable multimedia task-force",
+      "bs": "generate enterprise e-tailers"
+    }
+  },
+  {
+    "id": 8,
+    "name": "Nicholas Runolfsdottir V",
+    "username": "Maxime_Nienow",
+    "img" : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Pascale_fontenel_personne_16394.JPG/1200px-Pascale_fontenel_personne_16394.JPG",
+    "salary" : 500000,
+    "email": "Sherwood@rosamond.me",
+    "address": {
+      "street": "Ellsworth Summit",
+      "suite": "Suite 729",
+      "city": "Aliyaview",
+      "zipcode": "45169",
+      "geo": {
+        "lat": "-14.3990",
+        "lng": "-120.7677"
+      }
+    },
+    "phone": "586.493.6943 x140",
+    "website": "jacynthe.com",
+    "company": {
+      "name": "Abernathy Group",
+      "catchPhrase": "Implemented secondary concept",
+      "bs": "e-enable extensible e-tailers"
+    }
+  },
+  {
+    "id": 9,
+    "name": "Glenna Reichert",
+    "username": "Delphine",
+    "img" : "https://i.ytimg.com/vi/B6rcGplN9gg/maxresdefault.jpg",
+    "salary" : 500000,
+    "email": "Chaim_McDermott@dana.io",
+    "address": {
+      "street": "Dayna Park",
+      "suite": "Suite 449",
+      "city": "Bartholomebury",
+      "zipcode": "76495-3109",
+      "geo": {
+        "lat": "24.6463",
+        "lng": "-168.8889"
+      }
+    },
+    "phone": "(775)976-6794 x41206",
+    "website": "conrad.com",
+    "company": {
+      "name": "Yost and Sons",
+      "catchPhrase": "Switchable contextually-based project",
+      "bs": "aggregate real-time technologies"
+    }
+  },
+  {
+    "id": 10,
+    "name": "Clementina DuBuque",
+    "username": "Moriah.Stanton",
+    "img" : "http://www.twohandstech.com/wp-content/uploads/2013/10/frustrated-businessman.png",
+    "salary" : 500000,
+    "email": "Rey.Padberg@karina.biz",
+    "address": {
+      "street": "Kattie Turnpike",
+      "suite": "Suite 198",
+      "city": "Lebsackbury",
+      "zipcode": "31428-2261",
+      "geo": {
+        "lat": "-38.2386",
+        "lng": "57.2232"
+      }
+    },
+    "phone": "024-648-3804",
+    "website": "ambrose.net",
+    "company": {
+      "name": "Hoeger LLC",
+      "catchPhrase": "Centralized empowering task-force",
+      "bs": "target testing"
+    }
+  },
+  {
+    "id": 11,
+    "name": "Concepcion Bailey",
+    "username": "Concepcion.Bailey",
+    "img" : "https://www.scienceabc.com/wp-content/uploads/2015/11/singing-bad.jpg",
+    "salary" : 5000000,
+    "email": "concepcionbailey@manufact.com",
+    "address": {
+      "street": "Kattie Tuarnpike",
+      "suite": "Suite 298",
+      "city": "California",
+      "zipcode": "31428-261",
+      "geo": {
+        "lat": "-38.386",
+        "lng": "5.2232"
+      }
+    },
+    "phone": "+1 (990) 457-2516",
+    "website": "bailey.net",
+    "company": {
+      "name": "MANUFACT"
+    }
+  },
+  {
+    "id": 12,
+    "name": "Buckley Russell",
+    "username": "Buckley.Russell",
+    "img" : "http://www.myphotocentral.com/wp-content/uploads/2015/12/A-Few-Tips-on-Better-People-Photography.jpg",
+    "salary" : 5000000,
+    "email": "buckleyrussell@plasmosis.com",
+    "address": {
+      "street": "995 Cedar Street",
+      "suite": "Springhill",
+      "city": "Virginia",
+      "zipcode": "9880",
+      "geo": {
+        "lat": "-318.386",
+        "lng": "53.2232"
+      }
+    },
+    "phone": "+1 (992) 535-3749",
+    "website": "russell.net",
+    "company": {
+      "name": "PLASMOSIS"
+    }
+  },
+  {
+    "id": 13,
+    "name": "Madeline Nichols",
+    "username": "Madeline.Nichols",
+    "img" : "https://www.publicdomainpictures.net/pictures/210000/nahled/business-woman-with-a-headset-1488653362PBH.jpg",
+    "salary" : 5000000,
+    "email": "madelinenichols@printspan.com",
+    "address": {
+      "street": "360 Rapelye Street",
+      "suite": "Deseret",
+      "city": "Hawaii",
+      "zipcode": "7848",
+      "geo": {
+        "lat": "-28.326",
+        "lng": "56.932"
+      }
+    },
+    "phone": "+1 (937) 591-2894",
+    "website": "Nichols.net",
+    "company": {
+      "name": "PRINTSPAN"
+    }
+  },
+  {
+    "id": 14,
+    "name": "Lynn Ortega",
+    "username": "Lynn.Ortega",
+    "img" : "https://thumbs.dreamstime.com/z/portrait-attractive-businessman-standing-14242725.jpg",
+    "salary" : 5000000,
+    "email": "lynnortega@ozean.com",
+    "address": {
+      "street": "559 Oliver Street",
+      "suite": "Buxton",
+      "city": "Puerto Rico",
+      "zipcode": "4397",
+      "geo": {
+        "lat": "-288.36",
+        "lng": "96.93"
+      }
+    },
+    "phone": "+1 (837) 413-2777",
+    "website": "Ortega.com",
+    "company": {
+      "name": "OZEAN"
+    }
+  },
+  {
+    "id": 15,
+    "name": "Casey Rush",
+    "username": "Casey.Rush",
+    "img" : "https://img.pngio.com/businessman-png-download-businessman-png-1997_2134.png",
+    "salary" : 5000000,
+    "email": "caseyrush@netbook.com",
+    "address": {
+      "street": "203 Coffey Street",
+      "suite": "Starks",
+      "city": "North Dakota",
+      "zipcode": "4114",
+      "geo": {
+        "lat": "-88.86",
+        "lng": "66.63"
+      }
+    },
+    "phone": "+1 (997) 479-2055",
+    "website": "Rush.com",
+    "company": {
+      "name": "NETBOOK"
+    }
+  }
+]
+
+export default users;
